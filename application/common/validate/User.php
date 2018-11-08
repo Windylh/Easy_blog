1caf964d0b28648d8e637e0425814eb104ce9226
windylh <windylhpeng@foxmail.com>
2018-11-08 21:55:44 +0800

    bug fix

--- application/common/validate/User.php
+++ application/common/validate/User.php
@@ -13,10 +13,10 @@ use think\validate;
 class User extends Validate
 {
     protected $rule = [
-        'username|用户名' => 'require|unique:user|unique:admin|length:6,16',
-        'password|密码' => 'require|length:6,16',
-        'conpass|确认密码' => 'require|confirm:password|length:6,16',
-        'email|邮箱' => 'require|email|unique:user|length:6,32',
+        'username|用户名' => 'require|unique:user|unique:admin|length:4,16',
+        'password|密码' => 'require',
+        'conpass|确认密码' => 'require|confirm:password',
+        'email|邮箱' => 'require|email|unique:user|length:6:32',
         'verify|验证码' => 'require|captcha|length:0,4'
     ];
 
